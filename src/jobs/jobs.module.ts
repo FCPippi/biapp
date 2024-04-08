61e60b8295034fbcada3193ab1437ff2cef4065b
FCPippi <107051365+FCPippi@users.noreply.github.com>
2024-04-08 14:14:02 +0000

    (feat) applying middleware

--- src/jobs/jobs.module.ts
+++ src/jobs/jobs.module.ts
@@ -1,9 +1,16 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { JobsService } from './jobs.service';
 import { JobsController } from './jobs.controller';
+import { AuthMiddleware } from 'src/auth/auth.middleware';
 
 @Module({
   controllers: [JobsController],
   providers: [JobsService],
 })
-export class JobsModule {}
+export class JobsModule implements NestModule{public configure(consumer: MiddlewareConsumer) {
+  consumer
+    .apply(AuthMiddleware)
+    .forRoutes(
+      {path: 'articles', method: RequestMethod.POST},
+    )
+}}
