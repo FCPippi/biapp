8c586e42da9e852ca02bb79eb9e86502a03bf659
FCPippi <107051365+FCPippi@users.noreply.github.com>
2024-04-09 18:07:38 +0000

    (feat) bash scripts

---
+++ scripts/start-server.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+mkdir keys
+cd keys
+openssl genpkey -algorithm RSA -out private_key.pem -pkeyopt rsa_keygen_bits:2048
+openssl rsa -pubout -in private_key.pem -out public_key.pem
+cd ..
+npm run start
