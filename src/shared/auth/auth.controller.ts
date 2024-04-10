277d0a2cb67980ab7cbafe1a8162728d94e8c0c5
FCPippi <107051365+FCPippi@users.noreply.github.com>
2024-04-10 14:17:02 +0000

    (feat) auth controller

---
+++ src/shared/auth/auth.controller.ts
@@ -0,0 +1,53 @@
+import { PrismaService } from '../prisma/prisma.service';
+import {
+  Body,
+  Controller,
+  Post,
+  UnauthorizedException,
+  UsePipes,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { compare } from 'bcryptjs';
+import { z } from 'zod';
+import { ZodValidationPipe } from '../pipes/zod-validation-pipe';
+
+const authenticateBodySchema = z.object({
+  email: z.string().email(),
+  password: z.string(),
+});
+
+type AuthenticateBodySchema = z.infer<typeof authenticateBodySchema>;
+
+@Controller('/login')
+export class AuthController {
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+  ) {}
+
+  @Post()
+  @UsePipes(new ZodValidationPipe(authenticateBodySchema))
+  async handle(@Body() body: AuthenticateBodySchema) {
+    const { email, password } = body;
+
+    const user = await this.prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (!user || user.isDeleted) {
+      throw new UnauthorizedException('Credenciais inválidas!');
+    }
+
+    const validatePassword = await compare(password, user.password);
+
+    if (!validatePassword) {
+      throw new UnauthorizedException('Credenciais inválidas!');
+    }
+
+    const accessToken = this.jwtService.sign({
+      sub: user.id,
+    });
+
+    return { access_token: accessToken, user_role: user.role };
+  }
+}
