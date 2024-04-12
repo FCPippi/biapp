30fa8722c388ce879842732ba9762b8916fe55ae
FCPippi <francisco.pippi@edu.pucrs.br>
2024-04-12 18:04:54 -0300

    unused imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { JobPostsModule } from './job_posts/jobPosts.module';
-import { ChatGateway } from './chat/chat.gateway';
 import { JobRequestsModule } from './job_requests/jobRequests.module';
 import { ChatModule } from './chat/chat.module';
-import { AuthController } from './shared/auth/auth.controller';
 import { AuthModule } from './shared/auth/auth.module';
 
 @Module({
